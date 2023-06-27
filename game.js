c1a736303ed4b26b8b165721bf6ffc815a0e0d23
astutz <arnaud.stutz@neomanis.io>
2023-06-27 18:16:16 +0200

    :sparkles: refacto and add colors

--- game.js
+++ game.js
@@ -1,74 +1,78 @@
-const readline = require('readline-sync');
-const Game = require('./model/Game.js');
+const readline = require("readline-sync");
+const chalk = require("chalk");
+const Game = require("./model/Game.js");
 
 let alertInput = false;
 
-let scoreBoard = {
-    playerCount : 0,
-    computerCount : 0
+const scoreBoard = {
+  playerCount: 0,
+  computerCount: 0,
 };
 
 function render(myBoard) {
-    console.clear();
-    myBoard.forEach(line => {
-        console.log(line);
-    });
-};
+  console.clear();
+  console.log();
+  console.log(chalk.blue("-----------------"));
+  myBoard.forEach((line) => {
+    console.log(line);
+  });
+  console.log(chalk.blue("-----------------"));
+  console.log();
+}
 
 function newGame() {
-    const game = new Game();
-    while(game.win === false) {
-        if(game.turn % 2 === 0) {
-            render(game.board);
-            if(alertInput) {
-                console.log('Play on NUMPAD : from 1 to 9 !! Try again.');
-            };
-            console.log('Your numPad represents the board.');
-            var input = readline.question('Choose the key (from 1 to 9) you wanna play  :');
-            if(parseInt(input) > 0 && parseInt(input) < 10) {
-                alertInput = false;
-                var {x , y} = game.coordinates[parseInt(input)];
-                if(game.board[x][y] !== ' ') {
-                    console.log("Already played !! Pick another key");
-                } else {
-                    game.board[x][y] = game.player;
-                    game.win = game.testEndGameConditions('You');
-                    game.incrTurn();
-                };
-            } else {
-                alertInput = true;
-            };                  
-        } else {
-            let {x, y} = game.ia();
-            if(game.board[x][y] === ' ') {
-                game.board[x][y] = game.computer;
-                game.win = game.testEndGameConditions('Computer');
-                game.incrTurn();
-            }
-        }
-
-    }; 
-    render(game.board);
-    console.log(game.win,' won this game !!');
-    switch(game.win){
-        case 'You':
-            scoreBoard.playerCount++;
-            break
-        case 'Computer':
-            scoreBoard.computerCount++;
-            break
-    }
-    console.log('Your score : '+scoreBoard.playerCount);
-    console.log("Computer's score : "+scoreBoard.computerCount);
-    if (readline.keyInYN('Play again ?')) {
-        // 'Y' key was pressed.
-        newGame();
-      } else {
-        // Another key was pressed.
-        console.log('See you soon ! Bye :-)');
+  const game = new Game();
+  while (game.win === false) {
+    if (game.turn % 2 === 0) {
+      render(game.board);
+      if (alertInput) {
+        console.log("Play on NUMPAD : from 1 to 9 !! Try again.");
       }
-    
-};
+      console.log("Your numPad represents the board.");
+      var input = readline.question(
+        chalk.blue("Choose the key (from 1 to 9) you wanna play  :")
+      );
+      if (parseInt(input) > 0 && parseInt(input) < 10) {
+        alertInput = false;
+        var { x, y } = game.coordinates[parseInt(input)];
+        if (game.board[x][y] !== game.FREE_SLOT) {
+          console.log("Already played !! Pick another key");
+        } else {
+          game.board[x][y] = game.player;
+          game.win = game.testEndGameConditions(game.PLAYER);
+          game.incrTurn();
+        }
+      } else {
+        alertInput = true;
+      }
+    } else {
+      let { x, y } = game.ia();
+      if (game.board[x][y] === game.FREE_SLOT) {
+        game.board[x][y] = game.computer;
+        game.win = game.testEndGameConditions(game.IA);
+        game.incrTurn();
+      }
+    }
+  }
+  render(game.board);
+  console.log(game.win, " won this game !!");
+  switch (game.win) {
+    case "You":
+      scoreBoard.playerCount++;
+      break;
+    case "Computer":
+      scoreBoard.computerCount++;
+      break;
+  }
+  console.log(chalk.green("Your score : " + scoreBoard.playerCount));
+  console.log(chalk.red("Computer's score : " + scoreBoard.computerCount));
+  if (readline.keyInYN("Play again ?")) {
+    // 'Y' key was pressed.
+    newGame();
+  } else {
+    // Another key was pressed.
+    console.log(chalk.green("See you soon ! Bye :-)"));
+  }
+}
 
 newGame();
-
--- model/Game.js
+++ model/Game.js
@@ -1,183 +1,186 @@
 class Game {
-    constructor() {
-        this.board = [
-            [' ',' ',' '],
-            [' ',' ',' '],
-            [' ',' ',' '],
-        ];
-        this.coordinates = {
-            1: {x: 2, y: 0},
-            2: {x: 2, y: 1},
-            3: {x: 2, y: 2},
-            4: {x: 1, y: 0},
-            5: {x: 1, y: 1},
-            6: {x: 1, y: 2},
-            7: {x: 0, y: 0},
-            8: {x: 0, y: 1},
-            9: {x: 0, y: 2}
-        }
-        this.player = "X";
-        this.computer = "O";
-        this.win = false;
-        this.turn = 0;
+  constructor() {
+    this.IA = "Computer";
+    this.PLAYER = "You";
+    this.NOONE = "Nobody";
+    this.FREE_SLOT = " ";
+    this.board = [
+      [this.FREE_SLOT, this.FREE_SLOT, this.FREE_SLOT],
+      [this.FREE_SLOT, this.FREE_SLOT, this.FREE_SLOT],
+      [this.FREE_SLOT, this.FREE_SLOT, this.FREE_SLOT],
+    ];
+    this.coordinates = {
+      1: { x: 2, y: 0 },
+      2: { x: 2, y: 1 },
+      3: { x: 2, y: 2 },
+      4: { x: 1, y: 0 },
+      5: { x: 1, y: 1 },
+      6: { x: 1, y: 2 },
+      7: { x: 0, y: 0 },
+      8: { x: 0, y: 1 },
+      9: { x: 0, y: 2 },
+    };
+    this.player = "X";
+    this.computer = "O";
+    this.win = false;
+    this.turn = 0;
+    this.victoryConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+  }
+
+  getBoard() {
+    return this.board;
+  }
+
+  testEndGameConditions(currentPlayer) {
+    const currentResult = this.board.flat();
+    const gameEnd = this.victoryConditions.find(
+      (condition) =>
+        condition.every((i) => currentResult[i] === this.computer) ||
+        condition.every((i) => currentResult[i] === this.player)
+    );
+    if (gameEnd) {
+      return currentPlayer;
+    }
+    if (this.turn === 8) {
+      return this.NOONE;
+    }
+    return false;
+  }
+
+  incrTurn() {
+    this.turn++;
+  }
+
+  ia() {
+    //Strategic opening
+    if (this.board[1][1] === this.FREE_SLOT) {
+      return { x: 1, y: 1 };
+    }
+    if (
+      this.board[1][1] === this.player &&
+      this.board[2][2] === this.FREE_SLOT
+    ) {
+      return { x: 2, y: 2 };
+    }
+    if (
+      this.board[1][1] === this.player &&
+      this.board[2][2] === this.computer
+    ) {
+      if (
+        this.board[0][2] === this.player &&
+        this.board[2][0] === this.FREE_SLOT
+      ) {
+        return { x: 2, y: 0 };
+      } else if (
+        this.board[2][0] === this.player &&
+        this.board[0][2] === this.FREE_SLOT
+      ) {
+        return { x: 0, y: 2 };
+      }
     }
 
-    getBoard() {
-        return this.board;
+    //Check for player's chances
+    let rawDanger = [];
+    let colDanger = [];
+
+    for (let x = 0; x < this.board.length; x++) {
+      let raw = 0;
+      for (let y = 0; y < this.board.length; y++) {
+        if (this.board[x][y] === this.player) {
+          raw++;
+        }
+      }
+      rawDanger.push(raw);
+    }
+    for (let y = 0; y < this.board.length; y++) {
+      let col = 0;
+      for (let x = 0; x < this.board.length; x++) {
+        if (this.board[x][y] === this.player) {
+          col++;
+        }
+      }
+      colDanger.push(col);
     }
 
-    testEndGameConditions(currentPlayer) {
-        for(let line of this.board) {
-            if(
-                line[0] === line[1] && 
-                line[1] === line[2] && 
-                line[1] !== ' ' 
-            ) {
-                return currentPlayer;
-            };
-        };
-    
-        for(let i = 0; i < this.board.length; i++) {
-            if(
-                this.board[0][i] === this.board[1][i] && 
-                this.board[1][i] === this.board[2][i] && 
-                this.board[1][i] !== ' '
-            ) {
-                return currentPlayer;
-            }
-        };
-    
-        if(
-            (
-                (this.board[0][0] === this.board[1][1] && this.board[1][1] === this.board[2][2]) ||
-                (this.board[1][1] === this.board[0][2] && this.board[1][1] === this.board[2][0])
-            ) &&
-            this.board[1][1] !== ' '
-        ) {
-            return currentPlayer;
-        };
-    
-        if(this.turn === 8) {
-            return 'Nobody';
-        };
-        return false;
+    //Check for computer's chances
+    let myRaws = [];
+    let myCols = [];
+    for (let x = 0; x < this.board.length; x++) {
+      let raw = 0;
+      for (let y = 0; y < this.board.length; y++) {
+        if (this.board[x][y] === this.computer) {
+          raw++;
+        }
+      }
+      myRaws.push(raw);
+    }
+    for (let y = 0; y < this.board.length; y++) {
+      let col = 0;
+      for (let x = 0; x < this.board.length; x++) {
+        if (this.board[x][y] === this.computer) {
+          col++;
+        }
+      }
+      myCols.push(col);
     }
 
-    incrTurn() {
-        this.turn ++;
+    //Manage computer ATTACKS
+    let winX = myRaws.indexOf(2);
+    var xNb;
+    if (winX !== -1) {
+      xNb = winX;
+      for (let i = 0; i < this.board.length; i++) {
+        if (this.board[xNb][i] === this.FREE_SLOT) {
+          return { x: xNb, y: i };
+        }
+      }
     }
 
-    ia() {
-    
-        //Strategic opening
-        if(this.board[1][1] === ' ') {
-            return {x: 1, y: 1};
+    let winY = myCols.indexOf(2);
+    var yNb;
+    if (winY !== -1) {
+      yNb = winY;
+      for (let i = 0; i < this.board.length; i++) {
+        if (this.board[i][yNb] === this.FREE_SLOT) {
+          return { x: i, y: yNb };
         }
-        if(this.board[1][1] === this.player && this.board[2][2] === ' ') {
-            return {x: 2, y: 2};
-        }
-        if(this.board[1][1] === this.player && this.board[2][2] === this.computer) {
-            if(this.board[0][2] === this.player && this.board[2][0] === ' ') {
-                return {x:2 , y: 0};
-            }else if(this.board[2][0] === this.player && this.board[0][2] === ' ') {
-                return {x: 0, y: 2};
-            }
-        }
-    
-        //Check for player's chances
-        let rawDanger = [];
-        let colDanger = [];
-        
-        for(let x = 0; x < this.board.length; x++) {
-            let raw = 0;
-            for(let y = 0; y < this.board.length; y++) {
-                if(this.board[x][y] === this.player) {
-                    raw++;
-                }
-            }
-            rawDanger.push(raw);
-        };
-        for(let y = 0; y < this.board.length; y++) {
-            let col = 0;
-            for(let x = 0; x < this.board.length; x++) {
-                if(this.board[x][y] === this.player) {
-                    col++;
-                }
-            }
-            colDanger.push(col);
-        }
-    
-        //Check for computer's chances
-        let myRaws = [];
-        let myCols = [];
-        for(let x = 0; x < this.board.length; x++) {
-            let raw = 0;
-            for(let y = 0; y < this.board.length; y++) {
-                if(this.board[x][y] === this.computer) {
-                    raw++;
-                }
-            }
-            myRaws.push(raw);
-        };
-        for(let y = 0; y < this.board.length; y++) {
-            let col = 0;
-            for(let x = 0; x < this.board.length; x++) {           
-                if(this.board[x][y] === this.computer) {
-                    col++;
-                }
-            }
-            myCols.push(col);
-        }
-    
-        //Manage computer ATTACKS
-        let winX = myRaws.indexOf(2);
-        var xNb;
-        if(winX !== -1) {
-            xNb = winX;
-            for(let i = 0; i<this.board.length; i++) {
-                if(this.board[xNb][i] === ' ') {
-                    return {x: xNb, y: i};
-                }
-            }
-        }
-    
-        let winY = myCols.indexOf(2);
-        var yNb;
-        if(winY !== -1) {
-            yNb = winY;
-            for(let i = 0; i<this.board.length; i++) {
-                if(this.board[i][yNb] === ' ') {
-                    return {x: i, y: yNb};
-                }
-            }
-        }
-    
-        //Manage computer DEFENDS
-        let looseX = rawDanger.indexOf(2);
-        if(looseX !== -1) {
-            xNb = looseX;
-            for(let i = 0; i<this.board.length; i++) {
-                if(this.board[xNb][i] === ' ') {
-                    return {x: xNb, y: i};
-                }
-            }
-        }
-        let looseY = colDanger.indexOf(2);
-        if(looseY !== -1) {
-            yNb = looseY;
-            for(let i = 0; i<this.board.length; i++) {
-                if(this.board[i][yNb] === ' ') {
-                    return {x: i, y: yNb};
-                }
-            }
-        }
-    
-        //If no-one is winning => randomPlay !!
-        let xRand = Math.round(Math.random() * 2);
-        let yRand = Math.round(Math.random() * 2);
-        return {x: xRand, y: yRand};
-    } 
+      }
+    }
 
-};
+    //Manage computer DEFENDS
+    let looseX = rawDanger.indexOf(2);
+    if (looseX !== -1) {
+      xNb = looseX;
+      for (let i = 0; i < this.board.length; i++) {
+        if (this.board[xNb][i] === this.FREE_SLOT) {
+          return { x: xNb, y: i };
+        }
+      }
+    }
+    let looseY = colDanger.indexOf(2);
+    if (looseY !== -1) {
+      yNb = looseY;
+      for (let i = 0; i < this.board.length; i++) {
+        if (this.board[i][yNb] === this.FREE_SLOT) {
+          return { x: i, y: yNb };
+        }
+      }
+    }
+
+    //If no-one is winning => randomPlay !!
+    let xRand = Math.round(Math.random() * 2);
+    let yRand = Math.round(Math.random() * 2);
+    return { x: xRand, y: yRand };
+  }
+}
 
 module.exports = Game;
